1b2f9c14f0dc2eaa82d9f684cf652ca341827375
Nuvisa09 <nurnisa0922@gmail.com>
2024-09-30 21:42:15 +0700

    Ubah gambar

--- src/components/AboutComponent.vue
+++ src/components/AboutComponent.vue
@@ -9,7 +9,7 @@
                         <div class="bsb-circle border border-4 border-warning position-absolute top-50 start-10 translate-middle z-1"></div>
                         <div class="bsb-circle bg-primary position-absolute top-50 start-50 translate-middle" style="--bsb-cs: 460px;"></div>
                         <div class="bsb-circle border border-4 border-warning position-absolute top-10 end-0 z-1" style="--bsb-cs: 100px;"></div>
-                      <img class="position-relative z-2" width="100%" height="100%" loading="lazy" src="../assets/profile.png" alt="A Digital Agency Specialized in AI and Web 3.0">
+                      <img class="position-relative z-2 rounded-circle" width="100%" height="100%" loading="lazy" src="../assets/9.png" alt="A Digital Agency Specialized in AI and Web 3.0">
                     </div>
                 </div>
                 <div class="col-lg-6 p-3" style="text-align: left; align-items: center;">
